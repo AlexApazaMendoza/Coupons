556dfe8aa87ea7604b9406d28a51f926a0ed84b8
Alex <alexapazamendoza@gmail.com>
2024-01-10 14:25:17 -0500

    binding adapter

--- app/src/main/java/com/alpamedev/coupons/common/adapters/CouponBindingAdapter.kt
+++ app/src/main/java/com/alpamedev/coupons/common/adapters/CouponBindingAdapter.kt
@@ -1,1 +1,9 @@
 package com.alpamedev.coupons.common.adapters
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("isGone")
+fun bindIsGone(view: View, isGone:Boolean) {
+    view.visibility = if (isGone) View.GONE else View.VISIBLE
+}
