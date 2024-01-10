dd84a9cf19c18e77b152bd49a079fa3abe46ae11
Alex <alexapazamendoza@gmail.com>
2024-01-10 15:03:00 -0500

    ui

--- app/src/main/java/com/alpamedev/coupons/common/adapters/CouponBindingAdapter.kt
+++ app/src/main/java/com/alpamedev/coupons/common/adapters/CouponBindingAdapter.kt
@@ -1,9 +1,23 @@
 package com.alpamedev.coupons.common.adapters
 
+import android.graphics.Color
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.alpamedev.coupons.R
 
 @BindingAdapter("isGone")
 fun bindIsGone(view: View, isGone:Boolean) {
     view.visibility = if (isGone) View.GONE else View.VISIBLE
+}
+
+@BindingAdapter("couponTitle")
+fun setCouponTitle(view: TextView, isActive:Boolean) {
+    if (isActive) {
+        view.text = view.context.getString(R.string.main_text_title)
+        view.setTextColor(Color.GREEN)
+    } else {
+        view.text = view.context.getString(R.string.main_text_create_coupon_title)
+        view.setTextColor(Color.RED)
+    }
 }
