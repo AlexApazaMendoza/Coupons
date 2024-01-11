8542519f326a820d46abf901deafe77b0559d6ab
Alex <alexapazamendoza@gmail.com>
2024-01-11 09:45:09 -0500

    pruebas con expresso

--- app/src/androidTest/java/com/alpamedev/coupons/MainActivityCreateTest.kt
+++ app/src/androidTest/java/com/alpamedev/coupons/MainActivityCreateTest.kt
@@ -1,10 +1,19 @@
 package com.alpamedev.coupons
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.alpamedev.coupons.mainModule.view.MainActivity
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
@@ -13,4 +22,19 @@ class MainActivityCreateTest {
 
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun createCouponTest() {
+        val etCoupon = onView(withId(R.id.tieCoupon))
+        etCoupon.check(matches(withText("")))   //verifica que(view)(coincida(con el texto("")))
+
+        etCoupon.perform(click())  //hace click
+        etCoupon.perform(replaceText("welcome01"))    //escribe
+
+        val btnConsult = onView(withId(R.id.btnConsult))
+        btnConsult.perform(click())    //hace click
+
+        val btnCreate = onView(withId(R.id.btnCreate))
+        btnCreate.check(matches(isDisplayed()))    //verifica que(btn)(sea visible)
+    }
 }
