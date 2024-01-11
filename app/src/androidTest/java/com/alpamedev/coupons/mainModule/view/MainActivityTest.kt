f9be6eb22dc52cda67cc21cac35602493d76e65a
Alex <alexapazamendoza@gmail.com>
2024-01-11 10:56:41 -0500

    asistente de expresso

---
+++ app/src/androidTest/java/com/alpamedev/coupons/mainModule/view/MainActivityTest.kt
@@ -0,0 +1,114 @@
+package com.alpamedev.coupons.mainModule.view
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.alpamedev.coupons.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.tieCoupon), withContentDescription("Coupon"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.tilCoupon),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText("welcome01"), closeSoftKeyboard())
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btnConsult), withText("Consult"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val textInputEditText2 = onView(
+            allOf(
+                withId(R.id.tieDescription), withContentDescription("Description"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.tilDescription),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText2.perform(replaceText("test"), closeSoftKeyboard())
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.btnCreate), withText("Create Coupon"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+
+        //validar al snackbar
+        val snackbar = onView(withId(com.google.android.material.R.id.snackbar_text))
+        snackbar.check(matches(withText(R.string.main_save_success)))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
